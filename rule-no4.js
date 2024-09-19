0b699e106eedfe4924eefb1825cf2b3edb32116b
Faisal-pixel <adamsfaisal2003@gmail.com>
2024-09-19 15:28:31 +0100

    rule-no4

---
+++ rule-no4.js
@@ -0,0 +1,16 @@
+function printALlNumbersThenAllPairSums(numbers) {
+    console.log('these are the numbers:');
+
+    numbers.forEach(function(number) {
+        console.log(number);
+    });
+
+    console.log('and these are their sums:');
+    numbers.forEach(function(firstNumber) {
+        numbers.forEach(function(secondNumber) {
+            console.log(firstNumber + secondNumber);
+        });
+    });
+}
+
+printALlNumbersThenAllPairSums([1, 2, 3, 4, 5]);
