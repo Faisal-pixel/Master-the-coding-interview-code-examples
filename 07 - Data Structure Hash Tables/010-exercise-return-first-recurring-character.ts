0369c68a569acb3cde952952701d739b7fc7b4b1
Faisal-pixel <adamsfaisal2003@gmail.com>
2025-05-13 11:45:01 +0100

    I will complete this later

--- 07 - Data Structure Hash Tables/010-exercise-return-first-recurring-character.ts
+++ 07 - Data Structure Hash Tables/010-exercise-return-first-recurring-character.ts
@@ -15,4 +15,26 @@ const returnFirstRecurringCharacter = (incomingArray: number[]) => {
     return undefined;
 }
 
-console.log(returnFirstRecurringCharacter([]));
+const returnFirstRecurringCharacter2 = (incomingArray: number[]) => {
+    // Lets create an empty object where will be storing the members we have looped through
+    const tempObj = {};
+
+    // then we can loop through the array passing in the values into the object, and then before we pass in a member, we check if they exist
+    // If they dont exist, we pass in
+    // If they do, we basically just return the number and break out
+    for(let i = 0; i < incomingArray.length; i++) {
+        if(tempObj[incomingArray[i]] === undefined) {
+            tempObj[incomingArray[i]] = i;
+        } else {
+            tempObj[incomingArray[i]] = incomingArray[i];
+        }
+    }
+    
+    for (let x in tempObj) {
+        console.log(x)
+    }
+    
+    return undefined;
+}
+
+console.log(returnFirstRecurringCharacter2([2, 5, 1, 2, 3, 5, 1, 2, 4]));
