95690fc0829c83fc1a22f869af241815f7291b01
Faisal-pixel <adamsfaisal2003@gmail.com>
2025-05-21 10:25:02 +0100

    implemented insert, prepend, traverseUntilIndex and printList functions on my own

--- 08 - Data Strucutre Linked Lists/015-doubly-linked-lists.ts
+++ 08 - Data Strucutre Linked Lists/015-doubly-linked-lists.ts
@@ -11,7 +11,7 @@ class DoublyLinkedListNode {
 }
 
 class DoublyLinkedList {
-    head: {value: any, next: any | null, prev: null};
+    head: {value: any, next: any | null, prev: null | any};
     tail: {value: any, next: null | any, prev: any | null};
     length: number;
 
@@ -38,10 +38,78 @@ class DoublyLinkedList {
         this.tail = newDoublyLinkedListNode;
         this.length++;
     }
+
+    // Now for prepend
+    // Function receives value. We create a new node using our class
+    // We set newNode.next to this.head
+    // Then set this.head to the newNode
+    prepend(value: any) {
+        const newDoublyLinkedListNode = new DoublyLinkedListNode(value);
+        newDoublyLinkedListNode.next = this.head;
+        this.head.prev = newDoublyLinkedListNode;
+        this.head = newDoublyLinkedListNode;
+        this.length++
+    }
+
+    // We want tocreate an array, where will push the values.
+    // Then to loop through, normal way, currentNode = this.head; while currentNode !== null, we keep pushing the value into the array
+    // Then we set currentNode = currentNode.next
+    printList() {
+        const array: any[] = [];
+        let currentNode = this.head;
+        while (currentNode !== null) {
+            array.push(currentNode.value);
+            currentNode = currentNode.next;
+        }
+
+        return array;
+    }
+
+    // Traverse until index function
+    // SO basically, we create a counter variable and set to 0;
+    // Then we want to loop through and use the counter to know the position
+    // While counter !== 2, keep increasing counter and setting currentNode = currentNode.next. After we want to return currentNode
+    traverseUntilIndex(index: number) {
+        let counter = 0;
+        let currentNode = this.head;
+        while(counter !== index) {
+            currentNode = currentNode.next;
+            counter++
+        }
+
+        return currentNode;
+    };
+
+    // For the insert functiion, we are receiving index, and value
+    // First we create the node with the value;
+    // Then we need to traverse to the node before the index
+    // Then we want to hold the value of nodeBeforeIndex.next; So we can use this later.
+    // Then we want to set nodeBeforeIndex.next to our newNode
+    // Then we want to set newNode.prev to nodeBeforeIndex
+    // Then we want to set newNode.next to holdingNode
+    // Then we want to set holdingNode.prev to newNode
+
+    insert(index: number, value: any) {
+        const newDoublyLinkedListNode = new DoublyLinkedListNode(value);
+        const nodeBeforeIndex = this.traverseUntilIndex(index - 1);
+        const holdingNode = nodeBeforeIndex.next;
+        nodeBeforeIndex.next = newDoublyLinkedListNode;
+        newDoublyLinkedListNode.prev = nodeBeforeIndex;
+        newDoublyLinkedListNode.next = holdingNode;
+        holdingNode.prev = newDoublyLinkedListNode;
+        this.length++;
+    }
 }
 
 const myDoublyLinkedList = new DoublyLinkedList(10);
-myDoublyLinkedList.append(20)
-myDoublyLinkedList.append(30)
-myDoublyLinkedList.append(40)
+myDoublyLinkedList.append(20);
+myDoublyLinkedList.append(30);
+myDoublyLinkedList.append(40);
+myDoublyLinkedList.prepend(50);
+myDoublyLinkedList.prepend(60);
+myDoublyLinkedList.insert(2, 40);
+myDoublyLinkedList.insert(3, 30);
+myDoublyLinkedList.insert(4, 20);
+console.log(myDoublyLinkedList.printList());
+console.log(myDoublyLinkedList.traverseUntilIndex(2));
 console.log(myDoublyLinkedList);
