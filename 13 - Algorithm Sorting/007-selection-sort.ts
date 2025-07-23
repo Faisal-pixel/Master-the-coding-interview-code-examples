064be087685439ce57c70702d536e3dee4e71a9d
Faisal-pixel <adamsfaisal2003@gmail.com>
2025-07-23 09:27:46 +0100

    i think i understand selection sort now

--- 13 - Algorithm Sorting/007-selection-sort.ts
+++ 13 - Algorithm Sorting/007-selection-sort.ts
@@ -34,5 +34,30 @@ function selectSortByAndrei (array: number[]) {
     }
 }
 
-selectSortByAndrei(selectionSortNumbers);
+// selectSortByAndrei(selectionSortNumbers);
+// console.log(selectionSortNumbers);
+
+function selectionSortRevision(array: number[]) {
+    for(let i = 0; i < array.length; i++) {
+        let smallestNumber = array[i];
+        let minIndex = i;
+
+        for(let j = i + 1; j < array.length; j++) {
+            if(smallestNumber > array[j]) {
+                smallestNumber = array[j];
+                minIndex = j;
+            }
+        }
+        
+        array[minIndex] = array[i];
+        array[i] = smallestNumber;
+    }
+}
+// So i thought, why cant we have O(n) for the best case scenario selection sort algorithm, and i realised something we always end up
+// have to go through the array even if the whole arrray is sorted, because if you try to check if the smallest number doesnt change
+// for one instance? It wont work because we can have an array like [0, 1, 2, 4, 3, 7, 8]
+// Let's say array[n] = 2; We still need to use the n to check everything after that till the end, even though the smallest number
+// is still 2, because the remainnig guys arent exactly sorted.
+
+selectionSortRevision(selectionSortNumbers);
 console.log(selectionSortNumbers);
